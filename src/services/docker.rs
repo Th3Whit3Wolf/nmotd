70e574e440148d7b8b08fede9da61a2284ca4a60
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2020-04-21 20:46:05 +0100

    Better docker formatting

--- src/services/docker.rs
+++ src/services/docker.rs
@@ -2,12 +2,19 @@ use std::process::Command;
 
 pub fn get_docker_processes() -> Option<Vec<(String, String)>> {
     let mut docker: Vec<(String, String)> = Vec::with_capacity(4);
-    match Command::new("docker").arg("ps").arg("--format").arg("{{.Image}}:#: {{.Status}}").output() {
+    match Command::new("docker")
+        .arg("ps")
+        .arg("--format")
+        .arg("{{.Image}}:#: {{.Status}}")
+        .output()
+    {
         Ok(x) => {
             for line in String::from_utf8(x.stdout).unwrap().lines().skip(1) {
-                
-                docker.push((line.split(":#:").next().unwrap().to_string(),line.split(":#:").nth(1).unwrap().to_string()))
-                }
+                docker.push((
+                    line.split(":#:").next().unwrap().to_string(),
+                    line.split(":#:").nth(1).unwrap().to_string(),
+                ))
+            }
             Some(docker)
         }
         Err(_) => None,
